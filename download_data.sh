898555577e09510e7a5dcb98058afd73c9238b04
Joyjit Daw <jdaw@nvidia.com>
2019-09-19 16:01:14 -0400

    [data] update download data script
    script was set to exit if pfam data was not downloaded. changed
    it to break out of while loop instead of exiting.

--- download_data.sh
+++ download_data.sh
@@ -4,7 +4,7 @@ while true; do
      read -p "Do you wish to download and unzip the pretraining corpus? It is 5GB compressed and 40GB uncompressed? [y/n]" yn
      case $yn in
 	    [Yy]* ) wget http://s3.amazonaws.com/proteindata/data/pfam.tar.gz; tar -xzf pfam.tar.gz -C ./data; rm pfam.tar.gz; break;;
-            [Nn]* ) exit;;
+            [Nn]* ) break;;
 	    * ) echo "Please answer yes (Y/y) or no (N/n).";;
     esac
 done
