96e78030b8728515063f0e590016d6764c82240c
Neil Thomas <thomas.a.neil@gmail.com>
2021-08-02 14:55:25 -0700

    Update data and model paths to new s3 bucket

--- download_data.sh
+++ download_data.sh
@@ -3,17 +3,17 @@ mkdir -p ./data
 while true; do
      read -p "Do you wish to download and unzip the pretraining corpus? It is 5GB compressed and 40GB uncompressed? [y/n]" yn
      case $yn in
-	    [Yy]* ) wget http://s3.amazonaws.com/proteindata/data/pfam.tar.gz; tar -xzf pfam.tar.gz -C ./data; rm pfam.tar.gz; break;;
+	    [Yy]* ) wget http://s3.amazonaws.com/songlabdata/proteindata/data/pfam.tar.gz; tar -xzf pfam.tar.gz -C ./data; rm pfam.tar.gz; break;;
             [Nn]* ) break;;
 	    * ) echo "Please answer yes (Y/y) or no (N/n).";;
     esac
 done
 
-wget http://s3.amazonaws.com/proteindata/data/secondary_structure.tar.gz
-wget http://s3.amazonaws.com/proteindata/data/proteinnet.tar.gz
-wget http://s3.amazonaws.com/proteindata/data/remote_homology.tar.gz
-wget http://s3.amazonaws.com/proteindata/data/fluorescence.tar.gz
-wget http://s3.amazonaws.com/proteindata/data/stability.tar.gz
+wget http://s3.amazonaws.com/songlabdata/proteindata/data/secondary_structure.tar.gz
+wget http://s3.amazonaws.com/songlabdata/proteindata/data/proteinnet.tar.gz
+wget http://s3.amazonaws.com/songlabdata/proteindata/data/remote_homology.tar.gz
+wget http://s3.amazonaws.com/songlabdata/proteindata/data/fluorescence.tar.gz
+wget http://s3.amazonaws.com/songlabdata/proteindata/data/stability.tar.gz
 
 tar -xzf secondary_structure.tar.gz -C ./data
 tar -xzf proteinnet.tar.gz -C ./data
--- download_pretrained_models.sh
+++ download_pretrained_models.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-wget http://s3.amazonaws.com/proteindata/pretrain_weights/lstm_weights.h5 -P ./pretrained_models
-wget http://s3.amazonaws.com/proteindata/pretrain_weights/transformer_weights.h5 -P ./pretrained_models
-wget http://s3.amazonaws.com/proteindata/pretrain_weights/resnet_weights.h5 -P ./pretrained_models
-wget http://s3.amazonaws.com/proteindata/pretrain_weights/unirep_weights.h5 -P ./pretrained_models
-wget http://s3.amazonaws.com/proteindata/pretrain_weights/bepler_unsupervised_pretrain_weights.h5 -P ./pretrained_models
-wget http://s3.amazonaws.com/proteindata/pretrain_weights/bepler_multitask_pretrain_weights.h5 -P ./pretrained_models
+wget http://s3.amazonaws.com/songlabdata/proteindata/pretrain_weights/lstm_weights.h5 -P ./pretrained_models
+wget http://s3.amazonaws.com/songlabdata/proteindata/pretrain_weights/transformer_weights.h5 -P ./pretrained_models
+wget http://s3.amazonaws.com/songlabdata/proteindata/pretrain_weights/resnet_weights.h5 -P ./pretrained_models
+wget http://s3.amazonaws.com/songlabdata/proteindata/pretrain_weights/unirep_weights.h5 -P ./pretrained_models
+wget http://s3.amazonaws.com/songlabdata/proteindata/pretrain_weights/bepler_unsupervised_pretrain_weights.h5 -P ./pretrained_models
+wget http://s3.amazonaws.com/songlabdata/proteindata/pretrain_weights/bepler_multitask_pretrain_weights.h5 -P ./pretrained_models
